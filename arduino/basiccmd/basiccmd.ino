5e1b1077aa01e26ebcd78471ebd937fe45de2f82
Thomas Helmkay <adeptues@googlemail.com>
2014-04-06 14:12:12 +0100

    merged sketches

--- arduino/basiccmd/basiccmd.ino
+++ arduino/basiccmd/basiccmd.ino
@@ -10,15 +10,30 @@
  data between Serial Monitor and bluetooth module.
  */
 #include <SoftwareSerial.h>  
-
+//POTENTIAL PIN CONFLICT
 int bluetoothTx = 2;  // TX-O pin of bluetooth mate, Arduino D2
 int bluetoothRx = 3;  // RX-I pin of bluetooth mate, Arduino D3
+const int pinADir = 12;
+const int pinBDir = 13;
+const int pwmA = 3;//analog CONFLICT
+const int pwmB = 11;//analog
+const int brakeA = 9;
+const int brakeB = 8;
+const int curA = 0;//analog input
+const int curB = 1;//analog input
 
 SoftwareSerial bluetooth(bluetoothTx, bluetoothRx);
 
 void setup()
 {
   Serial.begin(9600);  // Begin the serial monitor at 9600bps
+  
+  //Configure motor pins
+  pinMode(pinADir,OUTPUT);
+  //pwm speed
+  pinMode(pwmA,OUTPUT);//0 for stop by inertia 255 for max speed
+  pinMode(pinBDir,OUTPUT);
+  pinMode(pwmB,OUTPUT);
 
   bluetooth.begin(115200);  // The Bluetooth Mate defaults to 115200bps
   bluetooth.print("$");  // Print three times individually
@@ -28,6 +43,40 @@ void setup()
   bluetooth.println("U,9600,N");  // Temporarily Change the baudrate to 9600, no parity
   // 115200 can be too fast at times for NewSoftSerial to relay the data reliably
   bluetooth.begin(9600);  // Start bluetooth serial at 9600
+}
+
+//Move the robot forward both wheels
+void forward(){
+  digitalWrite(pinADir,HIGH);
+  digitalWrite(pinBDir,HIGH);
+  analogWrite(pwmA,255);
+  analogWrite(pwmB,255);
+}
+//Move the robot backwards both wheels
+void reverse(){
+  digitalWrite(pinADir,LOW);
+  digitalWrite(pinBDir,LOW);
+  analogWrite(pwmA,255);
+  analogWrite(pwmB,255);
+}
+
+void brakeInertia(){
+  analogWrite(pwmB,0);
+  analogWrite(pwmA,0);
+}
+//Move left spin motor B 
+void left(){
+  digitalWrite(pinBDir,LOW);
+  digitalWrite(pinADir,HIGH);
+  analogWrite(pwmB,255);
+  analogWrite(pwmA,255);
+}
+//Move right spin motor A
+void right(){
+  digitalWrite(pinBDir,HIGH);
+  digitalWrite(pinADir,LOW);
+  analogWrite(pwmB,255);
+  analogWrite(pwmA,255);
 }
 
 void loop()
@@ -46,13 +95,6 @@ void loop()
     }else if(command == 'b'){
       //brake
     }
-    // Send any characters the bluetooth prints to the serial monitor
-    Serial.print((char)bluetooth.read());  
+    //flush maybe  
   }
- /* if(Serial.available())  // If stuff was typed in the serial monitor
-  {
-    // Send any characters the Serial monitor prints to the bluetooth
-    bluetooth.print((char)Serial.read());
-  }*/
-  // and loop forever and ever!
 }
